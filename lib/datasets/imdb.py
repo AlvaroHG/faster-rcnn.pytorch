3d7abe92e45645e6736286edaca68932cb604270
AlvaroHG <alvaroh@MacBook-Pro-4.local>
2018-08-08 19:57:35 -0700

    Changes

--- lib/datasets/imdb.py
+++ lib/datasets/imdb.py
@@ -120,7 +120,7 @@ class imdb(object):
       oldx2 = boxes[:, 2].copy()
       boxes[:, 0] = widths[i] - oldx2 - 1
       boxes[:, 2] = widths[i] - oldx1 - 1
-      print("boxes + " + boxes)
+      print("boxes + {}".format(boxes))
       print(boxes[:, 2])
       print(boxes[:, 0])
       assert (boxes[:, 2] >= boxes[:, 0]).all()

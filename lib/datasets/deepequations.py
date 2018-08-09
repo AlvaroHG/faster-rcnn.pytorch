5282840da50c69f0b15390cd51d13fb515b2d947
AlvaroHG <alvaroh@MacBook-Pro-4.local>
2018-08-09 09:38:41 -0700

    Eval change

--- lib/datasets/deepequations.py
+++ lib/datasets/deepequations.py
@@ -293,7 +293,7 @@ class deepequations(imdb):
                                 format(str(index), dets[k, -1],
                                        dets[k, 0] + 1, dets[k, 1] + 1,
                                        dets[k, 2] + 1, dets[k, 3] + 1))
-                        
+
 
     def _do_python_eval(self, output_dir = 'output'):
         """"
@@ -429,8 +429,7 @@ class deepequations(imdb):
         status = subprocess.call(cmd, shell=True)
 
     def evaluate_detections(self, all_boxes, output_dir):
-
-        self._write_voc_results_file(all_boxes)
+        self._write_voc_results_file(self.classes, all_boxes, output_dir)
         self._do_python_eval(output_dir)
         if self.config['matlab_eval']:
             self._do_matlab_eval(output_dir)

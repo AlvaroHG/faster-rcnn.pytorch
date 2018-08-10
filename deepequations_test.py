dba1cad48070ef33a859e59116c2821c04860924
AlvaroHG <alvaroh@MacBook-Pro-4.local>
2018-08-09 23:35:56 -0700

    pickling

--- deepequations_test.py
+++ deepequations_test.py
@@ -30,6 +30,7 @@ from model.rpn.bbox_transform import bbox_transform_inv
 from model.utils.net_utils import save_net, load_net, vis_detections
 from model.faster_rcnn.vgg16 import vgg16
 from model.faster_rcnn.resnet import resnet
+import re
 
 from deepequations_eval import compute_pr_curve, compute_tokenwise_eval
 
@@ -217,104 +218,116 @@ if __name__ == '__main__':
     _t = {'im_detect': time.time(), 'misc': time.time()}
     det_file = os.path.join(output_dir, 'detections.pkl')
 
+    run_feedforward = True
+    if os.path.exists(det_file):
+        name = input(
+            "\nPicke file with detections '{}' already exists. Do you want to use it?\ny / n:\n".format(
+                det_file))
+        if re.match("[y]|(yes)$", name, re.IGNORECASE):
+            run_feedforward = False
+            with open(det_file, 'rb') as f:
+                all_boxes = pickle.load(f)
+
+
     fasterRCNN.eval()
     empty_array = np.transpose(np.array([[], [], [], [], []]), (1, 0))
-    for i in range(num_images):
+    if run_feedforward:
+        for i in range(num_images):
 
-        data = next(data_iter)
-        im_data.data.resize_(data[0].size()).copy_(data[0])
-        im_info.data.resize_(data[1].size()).copy_(data[1])
-        gt_boxes.data.resize_(data[2].size()).copy_(data[2])
-        num_boxes.data.resize_(data[3].size()).copy_(data[3])
+            data = next(data_iter)
+            im_data.data.resize_(data[0].size()).copy_(data[0])
+            im_info.data.resize_(data[1].size()).copy_(data[1])
+            gt_boxes.data.resize_(data[2].size()).copy_(data[2])
+            num_boxes.data.resize_(data[3].size()).copy_(data[3])
 
-        det_tic = time.time()
-        rois, cls_prob, bbox_pred, \
-        rpn_loss_cls, rpn_loss_box, \
-        RCNN_loss_cls, RCNN_loss_bbox, \
-        rois_label = fasterRCNN(im_data, im_info, gt_boxes, num_boxes)
+            det_tic = time.time()
+            rois, cls_prob, bbox_pred, \
+            rpn_loss_cls, rpn_loss_box, \
+            RCNN_loss_cls, RCNN_loss_bbox, \
+            rois_label = fasterRCNN(im_data, im_info, gt_boxes, num_boxes)
 
-        scores = cls_prob.data
-        boxes = rois.data[:, :, 1:5]
+            scores = cls_prob.data
+            boxes = rois.data[:, :, 1:5]
 
-        if cfg.TEST.BBOX_REG:
-            # Apply bounding-box regression deltas
-            box_deltas = bbox_pred.data
-            if cfg.TRAIN.BBOX_NORMALIZE_TARGETS_PRECOMPUTED:
-                # Optionally normalize targets by a precomputed mean and stdev
-                if args.class_agnostic:
-                    box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(
-                        cfg.TRAIN.BBOX_NORMALIZE_STDS).cuda() \
-                                 + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS).cuda()
-                    box_deltas = box_deltas.view(1, -1, 4)
-                else:
-                    box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(
-                        cfg.TRAIN.BBOX_NORMALIZE_STDS).cuda() \
-                                 + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS).cuda()
-                    box_deltas = box_deltas.view(1, -1, 4 * len(imdb.classes))
+            if cfg.TEST.BBOX_REG:
+                # Apply bounding-box regression deltas
+                box_deltas = bbox_pred.data
+                if cfg.TRAIN.BBOX_NORMALIZE_TARGETS_PRECOMPUTED:
+                    # Optionally normalize targets by a precomputed mean and stdev
+                    if args.class_agnostic:
+                        box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(
+                            cfg.TRAIN.BBOX_NORMALIZE_STDS).cuda() \
+                                     + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS).cuda()
+                        box_deltas = box_deltas.view(1, -1, 4)
+                    else:
+                        box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(
+                            cfg.TRAIN.BBOX_NORMALIZE_STDS).cuda() \
+                                     + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS).cuda()
+                        box_deltas = box_deltas.view(1, -1, 4 * len(imdb.classes))
 
-            pred_boxes = bbox_transform_inv(boxes, box_deltas, 1)
-            pred_boxes = clip_boxes(pred_boxes, im_info.data, 1)
-        else:
-            # Simply repeat the boxes, once for each class
-            pred_boxes = np.tile(boxes, (1, scores.shape[1]))
-
-        pred_boxes /= data[1][0][2]
-
-        scores = scores.squeeze()
-        pred_boxes = pred_boxes.squeeze()
-        det_toc = time.time()
-        detect_time = det_toc - det_tic
-        misc_tic = time.time()
-        if vis:
-            im = cv2.imread(imdb.image_path_at(i))
-            im2show = np.copy(im)
-        for j in xrange(1, imdb.num_classes):
-            inds = torch.nonzero(scores[:, j] > thresh).view(-1)
-            # if there is det
-            if inds.numel() > 0:
-                cls_scores = scores[:, j][inds]
-                _, order = torch.sort(cls_scores, 0, True)
-                if args.class_agnostic:
-                    cls_boxes = pred_boxes[inds, :]
-                else:
-                    cls_boxes = pred_boxes[inds][:, j * 4:(j + 1) * 4]
-
-                cls_dets = torch.cat((cls_boxes, cls_scores.unsqueeze(1)), 1)
-                # cls_dets = torch.cat((cls_boxes, cls_scores), 1)
-                cls_dets = cls_dets[order]
-                keep = nms(cls_dets, cfg.TEST.NMS)
-                cls_dets = cls_dets[keep.view(-1).long()]
-                if vis:
-                    im2show = vis_detections(im2show, imdb.classes[j], cls_dets.cpu().numpy(), 0.3)
-                all_boxes[j][i] = cls_dets.cpu().numpy()
+                pred_boxes = bbox_transform_inv(boxes, box_deltas, 1)
+                pred_boxes = clip_boxes(pred_boxes, im_info.data, 1)
             else:
-                all_boxes[j][i] = empty_array
+                # Simply repeat the boxes, once for each class
+                pred_boxes = np.tile(boxes, (1, scores.shape[1]))
 
-        # Limit to max_per_image detections *over all classes*
-        if max_per_image > 0:
-            image_scores = np.hstack([all_boxes[j][i][:, -1]
-                                      for j in xrange(1, imdb.num_classes)])
-            if len(image_scores) > max_per_image:
-                image_thresh = np.sort(image_scores)[-max_per_image]
-                for j in xrange(1, imdb.num_classes):
-                    keep = np.where(all_boxes[j][i][:, -1] >= image_thresh)[0]
-                    all_boxes[j][i] = all_boxes[j][i][keep, :]
+            pred_boxes /= data[1][0][2]
 
-        misc_toc = time.time()
-        nms_time = misc_toc - misc_tic
+            scores = scores.squeeze()
+            pred_boxes = pred_boxes.squeeze()
+            det_toc = time.time()
+            detect_time = det_toc - det_tic
+            misc_tic = time.time()
+            if vis:
+                im = cv2.imread(imdb.image_path_at(i))
+                im2show = np.copy(im)
+            for j in xrange(1, imdb.num_classes):
+                inds = torch.nonzero(scores[:, j] > thresh).view(-1)
+                # if there is det
+                if inds.numel() > 0:
+                    cls_scores = scores[:, j][inds]
+                    _, order = torch.sort(cls_scores, 0, True)
+                    if args.class_agnostic:
+                        cls_boxes = pred_boxes[inds, :]
+                    else:
+                        cls_boxes = pred_boxes[inds][:, j * 4:(j + 1) * 4]
 
-        sys.stdout.write('im_detect: {:d}/{:d} {:.3f}s {:.3f}s   \r' \
-                         .format(i + 1, num_images, detect_time, nms_time))
-        sys.stdout.flush()
+                    cls_dets = torch.cat((cls_boxes, cls_scores.unsqueeze(1)), 1)
+                    # cls_dets = torch.cat((cls_boxes, cls_scores), 1)
+                    cls_dets = cls_dets[order]
+                    keep = nms(cls_dets, cfg.TEST.NMS)
+                    cls_dets = cls_dets[keep.view(-1).long()]
+                    if vis:
+                        im2show = vis_detections(im2show, imdb.classes[j], cls_dets.cpu().numpy(), 0.3)
+                    all_boxes[j][i] = cls_dets.cpu().numpy()
+                else:
+                    all_boxes[j][i] = empty_array
 
-        if vis:
-            cv2.imwrite('result.png', im2show)
-            pdb.set_trace()
-            # cv2.imshow('test', im2show)
-            # cv2.waitKey(0)
+            # Limit to max_per_image detections *over all classes*
+            if max_per_image > 0:
+                image_scores = np.hstack([all_boxes[j][i][:, -1]
+                                          for j in xrange(1, imdb.num_classes)])
+                if len(image_scores) > max_per_image:
+                    image_thresh = np.sort(image_scores)[-max_per_image]
+                    for j in xrange(1, imdb.num_classes):
+                        keep = np.where(all_boxes[j][i][:, -1] >= image_thresh)[0]
+                        all_boxes[j][i] = all_boxes[j][i][keep, :]
 
-    with open(det_file, 'wb') as f:
-        pickle.dump(all_boxes, f, pickle.HIGHEST_PROTOCOL)
+            misc_toc = time.time()
+            nms_time = misc_toc - misc_tic
+
+            sys.stdout.write('im_detect: {:d}/{:d} {:.3f}s {:.3f}s   \r' \
+                             .format(i + 1, num_images, detect_time, nms_time))
+            sys.stdout.flush()
+
+            if vis:
+                cv2.imwrite('result.png', im2show)
+                pdb.set_trace()
+                # cv2.imshow('test', im2show)
+                # cv2.waitKey(0)
+
+        with open(det_file, 'wb') as f:
+            pickle.dump(all_boxes, f, pickle.HIGHEST_PROTOCOL)
 
 
     img_names = [imdb.image_id_at(i) for i in range(imdb.num_images)]
@@ -347,7 +360,9 @@ if __name__ == '__main__':
     foreground_classes = imdb.classes[1:]
     print("number of images", len(img_names))
     save_dir_pr_curve = os.path.join(output_dir, 'pr_curves')
-    compute_pr_curve(config, foreground_classes, img_filenames,
+    compute_pr_curve(config,
+                     foreground_classes,
+                     img_names,
                      imdb.annotations_file_data['objects'],
                      all_results,
                      save_dir_pr_curve,
